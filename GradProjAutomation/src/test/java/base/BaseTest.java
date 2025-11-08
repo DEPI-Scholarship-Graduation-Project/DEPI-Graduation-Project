4ca6ec23708fdaa9133544bfdfccb1001d90124f
Muhammed Emad El-dien <muhammad.emad1233@gmail.com>
2025-11-08 22:57:24 +0200

    Recreating the project and some Additions
    - Recreated the java-maven project due to some errors.
    - Added BaseTest and HomePage scripts.

---
+++ GradProjAutomation/src/test/java/base/BaseTest.java
@@ -0,0 +1,31 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import pages.HomePage;
+
+public class BaseTest {
+    protected WebDriver driver;
+    protected HomePage homePage;
+    String baseUrl = "https://demowebshop.tricentis.com/";
+
+    //private final Logger logger = LoggerFactory.getLogger(BaseTest.class) ;
+
+    @BeforeClass
+    public void setup() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        homePage = new HomePage(driver);
+    }
+
+    @BeforeMethod
+    public void goHome() { driver.get(baseUrl); }
+
+    @AfterClass
+    public void closeBrowser() { driver.quit(); }
+
+
+}
