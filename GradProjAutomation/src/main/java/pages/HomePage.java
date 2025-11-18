ea8ea5b45bdddfa6908453057d8ff98c84a5463a
Muhammed Emad El-dien <muhammad.emad1233@gmail.com>
2025-11-18 23:11:58 +0200

    Add LoginPage and RegisterPage, update HomePage actions
    - Created LoginPage and RegisterPage classes for page object model structure.
    - Updated HomePage to include locators and actions for navigating to login and register pages, improving test automation modularity.

--- GradProjAutomation/src/main/java/pages/HomePage.java
+++ GradProjAutomation/src/main/java/pages/HomePage.java
@@ -1,5 +1,6 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class HomePage {
@@ -8,6 +9,26 @@ public class HomePage {
     // Constructor
     public HomePage(WebDriver driver) {
         this.driver = driver;
+    }
+
+    //Locators
+    //Only in Guest Mode
+    private By registerLink = By.linkText("Register");
+    private By loginLink = By.linkText("Log in");
+    //Only in Logged-in Mode
+    //Other Locators can be added here
+
+    //Actions
+    public RegisterPage clickOnRegisterLink()
+    {
+        driver.findElement(registerLink).click();
+        return new RegisterPage(driver);
+    }
+
+    public LoginPage clickOnLoginPage()
+    {
+        driver.findElement(loginLink).click();
+        return new LoginPage(driver);
     }
 
 }
---
+++ GradProjAutomation/src/main/java/pages/LoginPage.java
@@ -0,0 +1,40 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+    WebDriver driver;
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    //Locators
+    private By emailBox = By.xpath("//input[@id='Email']");
+    private By passwordBox = By.xpath("//input[@id='Password']");
+    private By rememberMeCheckBox = By.xpath("//input[@id='RememberMe']");
+    private By forgotPasswordLink = By.linkText("Forgot password?");
+    private By loginButton = By.xpath("//input[@value='Log in']");
+
+    //Actions
+    public void insertEmail(String email)
+    {
+        driver.findElement(emailBox).sendKeys(email);
+    }
+
+    public void insertPassword(String password)
+    {
+        driver.findElement(passwordBox).sendKeys(password);
+    }
+
+    public void checkRememberMeBox()
+    {
+        driver.findElement(rememberMeCheckBox).click();
+    }
+
+    public HomePage clickOnLoginButton()
+    {
+        driver.findElement(loginButton).click();
+        return new HomePage(driver);
+    }
+}
